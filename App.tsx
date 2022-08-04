8daf5b1b764e3b94833c2969b637bbd5e5e9f9d7
jacobcwright <jacob.wright.ut@gmail.com>
2022-08-03 20:45:11 -0600

    feat: navigation

--- App.tsx
+++ App.tsx
@@ -1,110 +1,46 @@
-import { Camera } from "expo-camera";
-import { useRef, useState } from "react";
-import {
-  StyleSheet,
-  Text,
-  View,
-  Button,
-  ImageBackground,
-  TouchableOpacity,
-} from "react-native";
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View, Button } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import MyCamera from "./Camera";
 
-export default function App() {
-  const [status, requestPermission] = Camera.useCameraPermissions();
-  const [type, setType] = useState(2);
-  const [lastPhotoURI, setLastPhotoURI] = useState(null);
-  const cameraRef = useRef(null);
-
-  if (!status?.granted) {
-    return (
-      <View
-        style={{ flex: 1, justifyContent: "center", alignContent: "center" }}
-      >
-        <Text style={{ textAlign: "center" }}>
-          We need access to your camera
-        </Text>
-        <Button onPress={requestPermission} title="Grant permission" />
-      </View>
-    );
-  }
-
-  if (lastPhotoURI !== null) {
-    return (
-      <ImageBackground
-        source={{ uri: lastPhotoURI }}
-        style={{
-          flex: 1,
-          backgroundColor: "transparent",
-          flexDirection: "row",
-          justifyContent: "center",
-        }}
-      >
-        <TouchableOpacity
-          style={{
-            flex: 0.2,
-            alignSelf: "flex-end",
-            alignItems: "center",
-            justifyContent: "center",
-            backgroundColor: "#666",
-            marginBottom: 40,
-            marginLeft: 20,
-          }}
-          onPress={() => {
-            setLastPhotoURI(null);
-          }}
-        >
-          <Text style={{ fontSize: 30, padding: 10, color: "white" }}>❌</Text>
-        </TouchableOpacity>
-      </ImageBackground>
-    );
-  }
-
+function HomeScreen({ navigation }: any) {
   return (
-    <Camera style={{ flex: 1 }} type={type} ref={cameraRef}>
-      <View
-        style={{
-          flex: 1,
-          backgroundColor: "transparent",
-          flexDirection: "row",
-          justifyContent: "center",
+    <View style={styles.container}>
+      <Text style={styles.title}>REAL dating</Text>
+      <Button
+        title="Log in"
+        onPress={() => {
+          navigation.navigate("Camera");
         }}
-      >
-        <TouchableOpacity
-          style={{
-            flex: 0.2,
-            alignSelf: "flex-end",
-            alignItems: "center",
-            justifyContent: "center",
-            backgroundColor: "#666",
-            marginBottom: 40,
-            marginLeft: 20,
-          }}
-          onPress={() => {
-            setType(type === 1 ? 2 : 1);
-          }}
-        >
-          <Text style={{ fontSize: 30, padding: 10, color: "white" }}>🔄</Text>
-        </TouchableOpacity>
-        <TouchableOpacity
-          style={{
-            flex: 0.2,
-            alignSelf: "flex-end",
-            alignItems: "center",
-            justifyContent: "center",
-            backgroundColor: "#666",
-            marginBottom: 40,
-            marginLeft: 20,
-          }}
-          onPress={async () => {
-            if (cameraRef.current) {
-              let photo = await (cameraRef.current as any).takePictureAsync();
-              setLastPhotoURI(photo.uri);
-            }
-          }}
-        >
-          <Text style={{ fontSize: 30, padding: 10, color: "white" }}>📸</Text>
-        </TouchableOpacity>
-      </View>
-    </Camera>
+      />
+      <StatusBar style="auto" />
+    </View>
   );
 }
+
+export default function App() {
+  const Stack = createNativeStackNavigator();
+
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="Camera" component={MyCamera} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "space-evenly",
+  },
+  title: {
+    fontSize: 30,
+    fontWeight: "bold",
+  },
+});
---
+++ Camera.tsx
@@ -0,0 +1,110 @@
+import { Camera } from "expo-camera";
+import { useRef, useState } from "react";
+import {
+  StyleSheet,
+  Text,
+  View,
+  Button,
+  ImageBackground,
+  TouchableOpacity,
+} from "react-native";
+
+export default function MyCamera() {
+  const [status, requestPermission] = Camera.useCameraPermissions();
+  const [type, setType] = useState(2);
+  const [lastPhotoURI, setLastPhotoURI] = useState(null);
+  const cameraRef = useRef(null);
+
+  if (!status?.granted) {
+    return (
+      <View
+        style={{ flex: 1, justifyContent: "center", alignContent: "center" }}
+      >
+        <Text style={{ textAlign: "center" }}>
+          We need access to your camera
+        </Text>
+        <Button onPress={requestPermission} title="Grant permission" />
+      </View>
+    );
+  }
+
+  if (lastPhotoURI !== null) {
+    return (
+      <ImageBackground
+        source={{ uri: lastPhotoURI }}
+        style={{
+          flex: 1,
+          backgroundColor: "transparent",
+          flexDirection: "row",
+          justifyContent: "center",
+        }}
+      >
+        <TouchableOpacity
+          style={{
+            flex: 0.2,
+            alignSelf: "flex-end",
+            alignItems: "center",
+            justifyContent: "center",
+            backgroundColor: "#666",
+            marginBottom: 40,
+            marginLeft: 20,
+          }}
+          onPress={() => {
+            setLastPhotoURI(null);
+          }}
+        >
+          <Text style={{ fontSize: 30, padding: 10, color: "white" }}>❌</Text>
+        </TouchableOpacity>
+      </ImageBackground>
+    );
+  }
+
+  return (
+    <Camera style={{ flex: 1 }} type={type} ref={cameraRef}>
+      <View
+        style={{
+          flex: 1,
+          backgroundColor: "transparent",
+          flexDirection: "row",
+          justifyContent: "center",
+        }}
+      >
+        <TouchableOpacity
+          style={{
+            flex: 0.2,
+            alignSelf: "flex-end",
+            alignItems: "center",
+            justifyContent: "center",
+            backgroundColor: "#666",
+            marginBottom: 40,
+            marginLeft: 20,
+          }}
+          onPress={() => {
+            setType(type === 1 ? 2 : 1);
+          }}
+        >
+          <Text style={{ fontSize: 30, padding: 10, color: "white" }}>🔄</Text>
+        </TouchableOpacity>
+        <TouchableOpacity
+          style={{
+            flex: 0.2,
+            alignSelf: "flex-end",
+            alignItems: "center",
+            justifyContent: "center",
+            backgroundColor: "#666",
+            marginBottom: 40,
+            marginLeft: 20,
+          }}
+          onPress={async () => {
+            if (cameraRef.current) {
+              let photo = await (cameraRef.current as any).takePictureAsync();
+              setLastPhotoURI(photo.uri);
+            }
+          }}
+        >
+          <Text style={{ fontSize: 30, padding: 10, color: "white" }}>📸</Text>
+        </TouchableOpacity>
+      </View>
+    </Camera>
+  );
+}
